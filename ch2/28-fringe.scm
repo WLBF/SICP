0690e66f1e7e2637a45568cb5209044808ff44ae
WLBF <lbf1353@live.com>
2015-04-01 23:05:51 +0800

    Update2.28

--- ch2/28-fringe.scm
+++ ch2/28-fringe.scm
@@ -1,11 +1,6 @@
-(define (fringe tree) (iter tree '()))
-   
-
-(define (iter rest result)
-     (cond ((null? rest)
-             result)
-          ((not (pair? (car rest)))
-            (iter (cdr rest) (append (list (car rest)) result)))
-          (else (iter (car rest) result))))
-          
-          ;;;Not done yet!!!!!
+(define (fringe tree)
+    (cond ((null? tree) '())
+          ((not (pair? tree)) (list tree))
+          (else (append (fringe (car tree))
+                        (fringe (cdr tree))))))
+       
