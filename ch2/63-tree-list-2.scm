d58ba8590a1ff58cc31045f87451d93d8fff25ca
WLBF <lbf1353@live.com>
2015-04-09 22:45:33 +0800

    2.65

---
+++ ch2/63-tree-list-2.scm
@@ -0,0 +1,11 @@
+(load "p106-tree.scm")
+
+(define (tree->list-2 tree)
+    (define (copy-to-list tree result-list)
+        (if (null? tree)
+            result-list
+            (copy-to-list (left-branch tree)
+                          (cons (entry tree)
+                                (copy-to-list (right-branch tree)
+                                              result-list)))))
+    (copy-to-list tree '()))
